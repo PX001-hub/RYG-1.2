cbc64952b56a35de260fafec39024b76e2277209
PX001-hub <draivv@yandex.ru>
2022-07-31 22:14:52 +0300

    Initial Commit

---
+++ RYG 1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RYG 1.2
+//
+//  Created by P Z on 31/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
