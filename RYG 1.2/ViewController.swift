51b5337d1d17ef2f423e8747cd83124d0df8cd62
PX001-hub <draivv@yandex.ru>
2022-07-31 23:55:59 +0300

    This is over 😱

--- RYG 1.2/ViewController.swift
+++ RYG 1.2/ViewController.swift
@@ -7,13 +7,42 @@
 
 import UIKit
 
+
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet var colorViewGreen: UIView!
+    @IBOutlet var colorViewPurple: UIView!
+    @IBOutlet var colorViewOrange: UIView!
+    
+    @IBOutlet var colorButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorViewGreen.layer.cornerRadius = 64
+        colorViewGreen.alpha = 0.5
+        
+        colorViewPurple.layer.cornerRadius = 64
+        colorViewPurple.alpha = 0.5
+        
+        colorViewOrange.layer.cornerRadius = 64
+        colorViewOrange.alpha = 0.5
+        
+        colorButton.tintColor = .white
+        colorButton.backgroundColor = #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1)
+        colorButton.layer.cornerRadius = 20
+        
     }
 
-
+    
+    @IBAction func tapColorsButton(_ sender: UIButton) {
+        
+        
+    }
+    
+    
 }
 
